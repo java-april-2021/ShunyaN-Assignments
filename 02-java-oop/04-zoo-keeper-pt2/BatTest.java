370fc80388c860b95ac6bbfcaf06ad93a3b5f304
Shunya Niizeki <shunyaniizeki1020@gmail.com>
2021-04-23 06:34:42 -0500

    Batman returns

---
+++ 02-java-oop/04-zoo-keeper-pt2/BatTest.java
@@ -0,0 +1,15 @@
+public class BatTest {
+    public static void main(String[] args){
+        Bat vania = new Bat();
+        vania.attackTown();
+        vania.attackTown();
+        vania.attackTown();
+        vania.eatHumans();
+        vania.eatHumans();
+        vania.eatHumans();
+        vania.eatHumans();
+        vania.fly();
+        vania.fly();
+        vania.displayEnergy();
+    }
+}
