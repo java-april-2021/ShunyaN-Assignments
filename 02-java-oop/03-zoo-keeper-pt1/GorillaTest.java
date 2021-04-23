5dfc01974e2dd948b3ba69345c4f8235cde546a8
Shunya Niizeki <shunyaniizeki1020@gmail.com>
2021-04-23 01:30:02 -0500

    Gorilla Poggers

---
+++ 02-java-oop/03-zoo-keeper-pt1/GorillaTest.java
@@ -0,0 +1,21 @@
+public class GorillaTest{
+    public static void main(String[] args){
+        Gorilla DK = new Gorilla();
+
+        DK.displayEnergy();
+        DK.throwSomething();
+        DK.throwSomething();
+        DK.throwSomething();
+        DK.throwSomething();
+        DK.throwSomething();
+        DK.displayEnergy();
+        DK.eatBananas();
+        DK.eatBananas();
+        DK.eatBananas();
+        DK.eatBananas();
+        DK.climb();
+        DK.climb();
+        DK.displayEnergy();
+    }
+
+}
